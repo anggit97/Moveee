6ac435eb67d117072631e216c3d95cedab934fbb
Anggit Prayogo <anggitprayogo@gmail.com>
2021-07-02 16:22:35 +0700

    Add module data, core, and model

---
+++ buildSrc/src/main/java/dependencies/Modules.kt
@@ -0,0 +1,13 @@
+package dependencies
+
+
+/**
+ * Created by Anggit Prayogo on 02,July,2021
+ * GitHub : https://github.com/anggit97
+ */
+object Modules {
+    const val App = ":app"
+    const val Data = ":data"
+    const val Core = ":core"
+    const val Model = ":model"
+}
