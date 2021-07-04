ebfa886a5ae078325ea12879a4a4a99cb207f5e3
Anggit Prayogo <anggitprayogo@gmail.com>
2021-07-04 07:17:46 +0700

    Add module home, map, search, settings

--- buildSrc/src/main/java/dependencies/Modules.kt
+++ buildSrc/src/main/java/dependencies/Modules.kt
@@ -6,8 +6,17 @@ package dependencies
  * GitHub : https://github.com/anggit97
  */
 object Modules {
+    //app
     const val App = ":app"
+
+    //abstraction
     const val Data = ":data"
     const val Core = ":core"
     const val Model = ":model"
+
+    //feature
+    const val Home = ":feature:home"
+    const val Search = ":feature:search"
+    const val Map = ":feature:map"
+    const val Settings = ":feature:settings"
 }
